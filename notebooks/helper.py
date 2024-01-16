8faaf8fde3b60dc115fe80e25913a18b198fe014
jordanott <jordanott365@gmail.com>
2024-01-16 14:23:56 -0500

    Format helper; plotting

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -1,6 +1,24 @@
 import pandas as pd
 
 
+def format_year_month_table(df: pd.DataFrame, value_name: str = "Value"):
+    """
+    Format a DataFrame that has columns like ["Year", "Jan", "Feb", ..., "Dec"]
+
+    :param df: DataFrame to reformat
+    :param value_name: Name to give the value column for melting
+    :return: DataFrame with Date column and corresponding values
+    """
+    df = pd.melt(df, id_vars=["Year"], var_name="Month", value_name=value_name)
+
+    # Convert abbreviated month to int; combine into single data column
+    df["Month"] = pd.to_datetime(df['Month'], format='%b').dt.month
+    df["Date"] = pd.to_datetime(df[["Year", "Month"]].assign(DAY=1))
+    df = df.sort_values(by="Date").reset_index(drop=True)
+
+    return df
+
+
 def percent_change_relative_to(df: pd.DataFrame, date: str, column_name: str) -> pd.DataFrame:
     """
     Compute the percent change relative to a specific date
@@ -10,7 +28,11 @@ def percent_change_relative_to(df: pd.DataFrame, date: str, column_name: str) ->
     :param column_name: Name of the column to compute on
     :return: New DataFrame with a "Percent Change" column
     """
-    index = df[df.Date == date].index[0]
+    if "Date" in df.columns:
+        index = df[df.Date == date].index[0]
+    else:
+        index = df[df.Year == date].index[0]
+
     df_relative_to_date = df.iloc[index:].copy()
 
     start = df_relative_to_date[column_name].values[0]
